9b7f427fa36a07a28a5236e3fbad11bb2d35e991
dreessen-n <dreessen.edu@gmail.com>
2022-06-28 20:16:38 -0600

    Update working on process_money route

--- server.py
+++ server.py
@@ -5,26 +5,47 @@ from flask import Flask, render_template, render_template, request, redirect, se
 app = Flask(__name__)
 app.secret_key='Mind your pot of GOLD'
 
+# Creat a list to store plays
+play = []
+message = ""
+
 @app.route('/')
 def index():
     """Render the home page"""
     if 'your_gold' not in session:
         session['your_gold'] = 0
         session['gold_play'] = 0
-        session['num_farm'] = 3
-        session['num_cave'] = 3
-        session['num_house'] = 3
-        session['num_casino'] = 3 
-        # plays = []
+        session['has_gold'] = 0
     else:
         session['your_gold'] += session['gold_play']
-        session['num_farm'] = random.randint(1, 4)
-        session['num_cave'] = random.randint(1, 4)
-        session['num_house'] = random.randint(1, 4)
-        session['num_casino'] = random.randint(1, 8)
-        # x = datetime.datetime.now()
-        # plays.append(x.strftime("c"))
+        play.append(message)
     return render_template('index.html')
+
+@app.route('/process_money', methods=['POST'])
+def process():
+    """Process the play and calculate wins/loses"""
+    if request.form['name'] == 'farm':
+        session['gold_play'] = random.randint(10, 20)
+        session['has_gold'] = 1
+        message = f"<p class='text-succes'>Earned {session['gold_play']} from the farm. ({datetime.datetime.now()}"
+    elif request.form['name'] == 'cave':
+        session['gold_play'] = random.randint(5, 10)
+        session['has_gold'] = 1
+        message = f"<p class='text-succes'>Earned {session['gold_play']} from the cave. ({datetime.datetime.now()}"
+    elif request.form['name'] == 'house':
+        session['gold_play'] = random.randint(2, 5)
+        session['has_gold'] = 1
+        message = f"<p class='text-succes'>Earned {session['gold_play']} from the house. ({datetime.datetime.now()}"
+    else:
+        session['gold_play'] = random.randint(-50, 50)
+        session['has_gold'] = 1
+        if session['gold_play'] > 0:
+            message = f"<p class='text-succes'>Earned {session['gold_play']} from the casino. ({datetime.datetime.now()}"
+        else:
+            message = f"<p class='text-danger'>Entered a casino and lost {session['gold_play']} golds... ouch. ({datetime.datetime.now()}"
+    return redirect('/', message=message)
+
+
 
 @app.errorhandler(404)
 def page_not_found(e):
